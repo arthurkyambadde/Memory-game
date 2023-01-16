d92b71892856cdd06179875faeee73830258973c
arthurkyambadde <arthurkyambadde9@gmail.com>
2023-01-16 12:54:12 +0300

    feat: rebuild

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cc6f34ca1fe3feb2"; }
+/******/ 		__webpack_require__.h = function() { return "3d864efb82221dd4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
